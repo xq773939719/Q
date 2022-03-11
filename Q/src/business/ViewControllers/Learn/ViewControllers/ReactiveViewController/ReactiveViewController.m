5214e9801d14dec533f053ed5940eb770032e105
晴风 <xuqiang01@bilibili.com>
2022-03-11 17:51:28 +0800

    rac

--- Q/src/business/ViewControllers/Learn/ViewControllers/ReactiveViewController/ReactiveViewController.m
+++ Q/src/business/ViewControllers/Learn/ViewControllers/ReactiveViewController/ReactiveViewController.m
@@ -7,15 +7,82 @@
 
 #import "ReactiveViewController.h"
 
-@interface ReactiveViewController ()
+@interface ReactiveViewController () <UITextFieldDelegate>
+
+@property (nonatomic, strong) UITextField *textField;
 
 @end
 
 @implementation ReactiveViewController
 
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+        [self setup];
+        [self observe];
+    }
+    return self;
+}
+
+- (void)loadView {
+    [super loadView];
+}
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+}
+
+- (void)viewWillLayoutSubviews {
+    [super viewWillLayoutSubviews];
+}
+
+- (void)viewDidLayoutSubviews {
+    [super viewDidLayoutSubviews];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+}
+
+- (void)viewDidDisappear:(BOOL)animated {
+    [super viewDidDisappear:animated];
+}
+
+- (void)setup {
+    [self.view addSubview:self.textField];
+    [self.textField mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(self.view).offset(100);
+        make.height.equalTo(@40);
+        make.width.equalTo(self.view).offset(-40);
+        make.centerX.equalTo(self.view);
+    }];
+}
+
+- (void)observe {
+    [[self.textField rac_textSignal] subscribeNext:^(NSString * _Nullable x) {
+        LoggerInfo(@"[ReactiveViewController]%s, %@", __func__, x);
+    }];
+}
+
+#pragma mark - privat method
+
+- (UITextField *)textField {
+    if (!_textField) {
+        _textField = [[UITextField alloc] init];
+        _textField.backgroundColor = [UIColor grayColor];
+        _textField.placeholder = @"默认";
+        _textField.adjustsFontSizeToFitWidth = YES;
+        _textField.minimumFontSize = 20;
+    }
+    return _textField;
 }
 
 @end
