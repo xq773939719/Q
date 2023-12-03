5e4a27bef0ab49288abbdf44e9d4bae2f3bcb070
晴风 <773939719@qq.com>
2023-12-03 22:43:30 +0800

    update

--- Q/main.m
+++ Q/main.m
@@ -11,10 +11,12 @@
 int main(int argc, char * argv[]) {
     static NSString * appDelegateClassName;
     static NSString * applicationClassName;
+    int ret = -1;
     @autoreleasepool {
         LoggerInfo(@"[main] -> %@", @"启动");
         applicationClassName = NSStringFromClass([UIApplication class]);
         appDelegateClassName = NSStringFromClass([BusinessApplicationDelegate class]);
+        ret = UIApplicationMain(argc, argv, applicationClassName, appDelegateClassName);
     }
-    return UIApplicationMain(argc, argv, applicationClassName, appDelegateClassName);
+    return ret;
 }
